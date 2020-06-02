8616cea34aec8fd0b3f10f5514b1a30a69a22b1f
Ralence <ranko.ostojic@gmail.com>
2020-06-02 13:54:47 +0200

    feat: add register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+  const { username, email, password, passwordConfirmation } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className="App">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" icon color="orange" textAlign="center">
+          <Icon name="puzzle piece" color="orange"></Icon>
+          Register for DevChat
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => handleChange(e)}
+            />
+            <Form.Input
+              fluid
+              icon="mail"
+              iconPosition="left"
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => handleChange(e)}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => handleChange(e)}
+            />
+            <Form.Input
+              fluid
+              icon="repeat"
+              iconPosition="left"
+              type="password"
+              name="passwordConfirmation"
+              placeholder="Confirm Password"
+              value={passwordConfirmation}
+              onChange={(e) => handleChange(e)}
+            />
+            <Button fluid color="orange" size="large">
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Already a user? <Link to="/login">Login</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Register;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./index.css";
+
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
+import "semantic-ui-css/semantic.min.css";
 
 const Root = () => {
   return (

08dc3527dc6258434f4ec15fecf62b25889362f0
Ralence <ranko.ostojic@gmail.com>
2020-06-08 19:39:27 +0200

    feat: create add channel modal

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,18 +1,70 @@
-import React, { useState } from "react";
-import { Menu, MenuItem, Icon } from "semantic-ui-react";
+import React, { useState, Fragment } from "react";
+import { Menu, MenuItem, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 
 const Channels = () => {
-  const [channels, setChannels] = useState([1, 1, 3]);
+  const [channels, setChannels] = useState([]);
+  const [showModal, setModal] = useState(false);
+  const [formData, setFormData] = useState({
+    channelName: "",
+    channelDetails: "",
+  });
+
+  const { channelName, channelDetails } = formData;
 
   return (
-    <Menu.Menu style={{ paddingBottom: "2em" }}>
-      <MenuItem>
-        <span>
-          <Icon name="exchange" /> CHANNELS
-        </span>{" "}
-        ({channels.length}) <Icon name="add" />
-      </MenuItem>
-    </Menu.Menu>
+    <Fragment>
+      <Menu.Menu style={{ paddingBottom: "2em" }}>
+        <MenuItem>
+          <span>
+            <Icon name="exchange" /> CHANNELS
+          </span>{" "}
+          ({channels.length}) <Icon name="add" onClick={() => setModal(true)} />
+        </MenuItem>
+      </Menu.Menu>
+      <Modal basic open={showModal} onClose={() => setModal(false)}>
+        <Modal.Header>Add a Channel</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Field>
+              <Input
+                fluid
+                label="Name of Channel"
+                name="channelName"
+                value={channelName}
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                fluid
+                label="Channel Details"
+                name="channelDetails"
+                value={channelDetails}
+                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
+              />
+            </Form.Field>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="green" inverted>
+            <Icon name="checkmark" /> Add
+          </Button>
+          <Button
+            color="red"
+            inverted
+            onClick={() => {
+              setModal(false);
+              setFormData({
+                channelName: "",
+                channelDetails: "",
+              });
+            }}
+          >
+            <Icon name="remove" /> Cancel
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </Fragment>
   );
 };
 

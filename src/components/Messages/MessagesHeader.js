84a26efffd003f98796f8ea5e5ec9f9373f38ca9
Ralence <ranko.ostojic@gmail.com>
2020-06-23 13:45:24 +0200

    feat: add dynamic chanal title and num of user

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,17 +1,54 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Header, Segment, Input, Icon } from "semantic-ui-react";
 import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
 
 const MessagesHeader = () => {
+  const { name } = useSelector(
+    (state) =>
+      state &&
+      state.messages &&
+      state.messages.currentChannel !== null &&
+      state.messages.currentChannel
+  );
+
+  const currentUser = useSelector(
+    (state) => state && state.auth && state.auth.user && state.auth.user
+  );
+
+  const loadedMessages = useSelector(
+    (state) =>
+      state && state.messages && state.messages.currentMessages && state.messages.currentMessages
+  );
+
+  const countUniqueUsers = (messages) => {
+    if (messages.length > 0) {
+      const uniqueUsers = messages.reduce((acc, message) => {
+        if (!acc.includes(currentUser.displayName)) {
+          acc.push(currentUser.displayName);
+        }
+        if (!acc.includes(message.user.name)) {
+          acc.push(message.user.name);
+        }
+        return acc;
+      }, []);
+      return uniqueUsers.length;
+    } else {
+      return 1;
+    }
+  };
+
   return (
     <Segment clearing>
       {/* Channel Title */}
       <Header as="h2" floated="left" style={{ marginBottom: "0" }}>
         <span>
-          Channel
+          {name ? `# ${name}` : "#"}
           <Icon name="star outline" color="black" />
         </span>
-        <HeaderSubHeader>2 users</HeaderSubHeader>
+        <HeaderSubHeader>{`${countUniqueUsers(loadedMessages)} user${
+          countUniqueUsers(loadedMessages) === 1 ? "" : "s"
+        }`}</HeaderSubHeader>
       </Header>
       {/* Channel Search*/}
       <Header floated="right">
